f361b631ec30213a6aa3021b5aeeaf70f6658a0e
mdemore2 <mark.demore2@gmail.com>
2020-05-26 14:19:25 -0400

    o okay now it works?

--- Homework/HW4/main.py
+++ Homework/HW4/main.py
@@ -20,21 +20,37 @@ def bk_pivot(graph, P, R=set(), X=set()):
     bk_pivot.count += 1
     if not any((P, X)):
         yield R
-    P_copy = P.copy()
-    pivot = random.choice(list(graph))
-    #pivot = int(str(random.sample(P_copy, 1)))
-    for node in P_copy.difference((graph.neighbors(pivot))):
-        for r in bk_pivot(graph, P.intersection(graph.neighbors(node)),
-                          R=R.union(set([node])), X=X.intersection(graph.neighbors(node))):
-            yield r
-        P.remove(node)
-        X.add(node)
+    else:
+        P_copy = P.copy()
+        pivot = random.choice(list(P.union(X)))
+        #pivot = int(str(random.sample(P_copy, 1)))
+        for node in P_copy.difference((graph.neighbors(pivot))):
+            for r in bk_pivot(graph, P.intersection(graph.neighbors(node)),
+                              R=R.union(set([node])), X=X.intersection(graph.neighbors(node))):
+                yield r
+            P.remove(node)
+            X.add(node)
 
+
+
+def bk_order_pivot(graph, P, R=set(), X=set()):
+    bk_order_pivot.count += 1
+    if not any((P, X)):
+        yield R
+    else:
+        P_copy = P.copy()
+        pivot = random.choice(list(P.union(X)))
+        #pivot = int(str(random.sample(P_copy, 1)))
+        for node in P_copy.difference((graph.neighbors(pivot))):
+            for r in bk_order_pivot(graph, P.intersection(graph.neighbors(node)),
+                              R=R.union(set([node])), X=X.intersection(graph.neighbors(node))):
+                yield r
+            P.remove(node)
+            X.add(node)
 
 def bk_order(graph, P, R=set(), X=set()):
-    bk_order.count += 1
     for node in P.copy():
-        for r in bk_pivot(graph, P.intersection(graph.neighbors(node)),
+        for r in bk_order_pivot(graph, P.intersection(graph.neighbors(node)),
                           R=R.union(set([node])), X=X.intersection(graph.neighbors(node))):
             yield r
         P.remove(node)
@@ -42,19 +58,13 @@ def bk_order(graph, P, R=set(), X=set()):
 
 
 if __name__ == '__main__':
-    max_cliques = []
 
-    R = nx.empty_graph()
-    X = nx.empty_graph()
-    P = nx.Graph()
-
-    P.add_edges_from([(1, 2), (1, 3), (2, 3), (2, 4), (2, 5), (3, 4),
-                      (4, 5), (4, 6), (5, 7), (5, 8), (7, 8)])
     graph = nx.Graph()
     graph.add_edges_from([(1, 2), (1, 3), (2, 3), (2, 4), (2, 5), (3, 4),
                           (4, 5), (4, 6), (5, 7), (5, 8), (7, 8)])
-    # complement P
-    # print(BK(R, P, X))
+
+    graph = nx.complement(graph)
+
     bk.count = 0
     print("Bron-Kerbosch Backtracking:")
     for max_clique in bk(graph, set(graph.nodes)):
@@ -65,9 +75,8 @@ if __name__ == '__main__':
     for max_clique in bk_pivot(graph, set(graph.nodes)):
         print(max_clique)
 
-    bk_order.count = 0
+    bk_order_pivot.count = 0
     print("Bron-Kerbosch Order:")
     for max_clique in bk_order(graph, set(nx.algorithms.coloring.strategy_largest_first(graph, 1))):
         print(max_clique)
 
-# find max  of max_cliques (G.order?)
